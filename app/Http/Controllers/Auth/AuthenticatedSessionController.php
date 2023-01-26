b2cb9cba604c2d0e94761b78f818bb5ee95165ff
Michael-Chacon <michaelchacon02@gmail.com>
2023-01-26 11:37:56 -0500

    Consultar credenciales de usuaro a la api desde el cliente y registrar los datos en la db del cliente

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\RedirectResponse;
+
+use App\Providers\RouteServiceProvider;
+use App\Http\Requests\Auth\LoginRequest;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -23,13 +26,33 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(Request $request)
     {
-        $request->authenticate();
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
 
-        $request->session()->regenerate();
+        $response = Http::withHeaders([
+            'Accept' => 'application/json'
+        ])->post('http://api.blog.test/v1/login', [
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+        
+        $response =  $response->json();
+        // return $response['data'];
+        $user = User::updateOrcreate([
+            'email' => $request->email
+        ],$response['data']);
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        return $user;
+
+        // $request->authenticate();
+
+        // $request->session()->regenerate();
+
+        // return redirect()->intended(RouteServiceProvider::HOME);
     }
 
     /**
